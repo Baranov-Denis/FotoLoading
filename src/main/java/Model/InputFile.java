398a9ed062a20daa6c80ee31466ec56c119822d6
Baranov_Denis <baranovdenis1983@gmail.com>
2021-03-30 15:05:34 +0300

    Changes to be committed: 	modified:   src/main/java/Model/InputFile.java
    	Fixed issue with wrong date.
    	Now program will gets date of when photos was captured.
    	Earlier it might be date of add change to the photo or some other date.

--- src/main/java/Model/InputFile.java
+++ src/main/java/Model/InputFile.java
@@ -7,6 +7,8 @@ import com.drew.metadata.Tag;
 
 import java.io.File;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputFile {
     private final File file;
@@ -14,7 +16,7 @@ public class InputFile {
     private String year;
     private String month;
     private String day;
-    private String time;
+   // private String time;
     private String type;
     private String absolutePathWithFileName;
     private String absolutePathOnlyFolders;
@@ -31,6 +33,10 @@ public class InputFile {
 
     public String getAbsolutePathOnlyFolders() {
         return absolutePathOnlyFolders;
+    }
+
+    public String getYear() {
+        return year;
     }
 
     public String getAbsolutePathWithFileName() {
@@ -65,10 +71,6 @@ public class InputFile {
     }
 
 
-    public void setTime(String time) {
-        this.time = time;
-    }
-
 
     public void setType(String type) {
         this.type = type;
@@ -80,12 +82,14 @@ public class InputFile {
 
 
     private void getInfoOfFile() {
-        try {
 
+
+
+        try {
             Metadata metadata;
 
-            //If length of file smaller than 50000 that its not a photo.
-            if (this.getFile().length() < 50000) return;
+            //If length of file smaller than 50000 that its not a photo. .cof is not photo but it will be copied.
+            if (this.getFile().length() < 50000 || this.getFile().getName().contains(".cof")) return;
 
             //Here checking existing large files like "TIF". When i tried read Metadata those files, i got OutOfMemory error. It issue in metadata extractor framework.
             //If length of file larger than 500000000 then metadata won't be able to read
@@ -106,6 +110,8 @@ public class InputFile {
 
                 for (Tag tag : directory.getTags()) {
 
+                   // System.out.println(tag.toString());
+
                     //-------------------------  Getting inputFile name. File name looks like fileName.jpg  ------
                     //--------------------------------------------------------------------------------------------
                     if (tag.toString().contains("[File] File Name")) {
@@ -115,13 +121,35 @@ public class InputFile {
 
                     //-------------------------  Getting inputFile Date in String  -------------------------------
                     //--------------------------------------------------------------------------------------------
-                    if (tag.toString().contains("File Modified Date")) {
+
+                    //"Date/Time Original" contains original date of when photo was created.
+                    // If file doesn't contains "Date/Time Original" then will be used info from "File Modified Date".
+                    // If "Date/Time Original" contains incorrect information then year will be set null and will be used info from "File Modified Date".
+                    // File might contains several  "File Modified Date" and "Date/Time Original".
+
+                    if(tag.toString().contains("Date/Time Original")){
+
+                        String[] tempAllDates = tag.toString().substring(35,45).split(":");
+
+                        Pattern pattern1 = Pattern.compile ("\\d{4}");
+                        Matcher matcher = pattern1.matcher(tempAllDates[0].trim());
+
+                            if(matcher.find()) {
+                            setYear(tempAllDates[0].trim());
+                        }else {
+                            setYear(null);
+                        }
+                        setMonth(tempAllDates[1].trim());
+                        setDay(tempAllDates[2].trim());
+
+                    }else if(tag.toString().contains("File Modified Date") && this.getYear() == null) {
                         String[] tempAllDates = tag.toString().substring(31).split(" ");
-                        setTime(tempAllDates[2].trim());
                         setDay(tempAllDates[1].trim());
                         setMonth(tempAllDates[0].replace(".", ""));
                         setYear(tempAllDates[4].trim());
                     }
+
+
 
                     //-------------------------  Getting inputFile Format in String  -----------------------------
                     //--------------------------------------------------------------------------------------------
@@ -189,28 +217,40 @@ public class InputFile {
 
     private String replaceMonthName(String month) {
         switch (month) {
+            case ("01"):
             case ("янв"):
                 return "01 Январь";
+            case ("02"):
             case ("февр"):
                 return "02 Февраль";
+            case ("03"):
             case ("мар"):
                 return "03 Март";
+            case ("04"):
             case ("апр"):
                 return "04 Апрель";
+            case ("05"):
             case ("мая"):
                 return "05 Май";
+            case ("06"):
             case ("июн"):
                 return "06 Июнь";
+            case ("07"):
             case ("июл"):
                 return "07 Июль";
+            case ("08"):
             case ("авг"):
                 return "08 Август";
+            case ("09"):
             case ("сент"):
                 return "09 Сентябрь";
+            case ("10"):
             case ("окт"):
                 return "10 Октябрь";
+            case ("11"):
             case ("нояб"):
                 return "11 Ноябрь";
+            case ("12"):
             case ("дек"):
                 return "12 Декабрь";
         }
@@ -222,11 +262,13 @@ public class InputFile {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         InputFile inputFile = (InputFile) o;
-        return Objects.equals(year, inputFile.year) && Objects.equals(month, inputFile.month) && Objects.equals(day, inputFile.day) && Objects.equals(time, inputFile.time) && Objects.equals(type, inputFile.type);
+        return Objects.equals(year, inputFile.year) && Objects.equals(month, inputFile.month) && Objects.equals(day,
+                inputFile.day) && Objects.equals(this.getFile().length(), inputFile.getFile().length()) && Objects.equals(type,
+                inputFile.type);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(year, month, day, time, type);
+        return Objects.hash(year, month, day, this.getFile().length(), type);
     }
 }
